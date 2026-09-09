32eb8ca6b546b9e0be1be13653464e5b15845bc1
Versus Admin <dev@versus.com>
2026-09-09 17:58:40 -0300

    fix: nestjs guard dependencies

--- backend/src/modules/contacts/contacts.module.ts
+++ backend/src/modules/contacts/contacts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContactsController } from './contacts.controller';
 import { ContactsService } from './contacts.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ContactsController],
   providers: [ContactsService],
 })
